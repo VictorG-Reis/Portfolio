20a2a005b62e9b6995d0cf5252799d3cef96021a
victor gabriel R <victorgrs11@outlook.com>
2024-03-18 12:52:56 -0300

    (feat) footer css

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="text-center bg-[#13161c] text-white">
+    <div className="text-center bg-[#13161c] text-white p-4">
       <a
+        target="_blank"
         href="https://github.com/VictorG-Reis"
-        className=" m-10"
+        rel="noreferrer"
       >
         Feito por Victor Gabriel .R
       </a>
